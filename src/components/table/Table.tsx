2f8f0d2dceb405cc4eb6a5da9b136130ec5bfaba
Alex Sneshko <007alex03@gmail.com>
2024-01-02 13:41:44 +0300

    feat: auto filtering

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -1,227 +1,350 @@
-import React, { useEffect, useState } from 'react'
-import csvToJson from 'csvtojson'
-import './Table.css'
-import Marquee from 'react-fast-marquee';
-import { AutopartForm } from '../autopartForm/AutopartForm';
+import React, { useEffect, useState } from "react";
+import csvToJson from "csvtojson";
+import "./Table.css";
+import Marquee from "react-fast-marquee";
+import { AutopartForm } from "../autopartForm/AutopartForm";
 
-const URL = 'https://docs.google.com/spreadsheets/d/e/' + process.env.REACT_APP_SHEET_ID + '/pub?output=csv';
+const URL =
+  "https://docs.google.com/spreadsheets/d/e/" +
+  process.env.REACT_APP_SHEET_ID +
+  "/pub?output=csv";
 
 export const Table = () => {
-
   interface ApiData {
-    "артикул": string
-    "описание": string
-    "category": string
-    "кол-во": string
-    "ц": string
-    "аналоги": string
-    "sale": string
+    артикул: string;
+    описание: string;
+    category: string;
+    "кол-во": string;
+    ц: string;
+    аналоги: string;
+    sale: string;
+    auto: string;
   }
 
   interface ApiAdditioanlData {
-    courses: string
-    text: string
+    courses: string;
+    text: string;
   }
 
   interface BankData {
-    Cur_ID: number,
-    Date: string,
-    Cur_Abbreviation: string,
-    Cur_Scale: number,
-    Cur_Name: string,
-    Cur_OfficialRate: number
+    Cur_ID: number;
+    Date: string;
+    Cur_Abbreviation: string;
+    Cur_Scale: number;
+    Cur_Name: string;
+    Cur_OfficialRate: number;
   }
 
-  const [data, setData] = useState<Array<ApiData>>()
-  const [filteredData, setFilteredData] = useState<Array<ApiData>>()
-  const [selectedCategories, setSelectedCategories] = useState<Array<string>>([])
-  const [categories, setCategories] = useState<Array<string>>([])
-  const [showFiltersCheckbox, setShowFiltersCheckbox] = useState<boolean>(false)
-  const [sale, setSale] = useState<boolean>(false)
-  const [markdown, setMarkdown] = useState<boolean>(false)
-  const [currency, setCurrency] = useState<BankData>()
-  const [courses, setCourses] = useState<string>()
-  const [adText, setAdText] = useState<string>()
-  const [showProductForm, setShowProductForm] = useState<boolean>(false)
+  const [data, setData] = useState<Array<ApiData>>();
+  const [filteredData, setFilteredData] = useState<Array<ApiData>>();
+  const [selectedCategories, setSelectedCategories] = useState<Array<string>>(
+    []
+  );
+  const [categories, setCategories] = useState<Array<string>>([]);
+  const [autoMarks, setAutoMarks] = useState<Array<string>>([]);
+  const [showFiltersCheckbox, setShowFiltersCheckbox] =
+    useState<boolean>(false);
+  const [sale, setSale] = useState<boolean>(false);
+  const [markdown, setMarkdown] = useState<boolean>(false);
+  const [currency, setCurrency] = useState<BankData>();
+  const [courses, setCourses] = useState<string>();
+  const [adText, setAdText] = useState<string>();
+  const [showProductForm, setShowProductForm] = useState<boolean>(false);
 
   type StockOption = "акция" | "уценка";
 
   const loadDataFromSheet = async (URL: string) => {
     const dataUrl = await fetch(URL);
-    const res = await dataUrl.text()
-    const json: Array<ApiData> = await csvToJson().fromString(res)
+    const res = await dataUrl.text();
+    const json: Array<ApiData> = await csvToJson().fromString(res);
 
-    setData(json)
-    setFilteredData(json)
-    setCategories(getAllCategories(json))
+    setData(json);
+    setFilteredData(json);
+    setCategories(getAllCategories(json));
+    setAutoMarks(getAllAutoMarks(json));
 
-    const additionalJson: Array<ApiAdditioanlData> = await csvToJson().fromString(res)
-    setCourses(additionalJson[0].courses)
-    setAdText(additionalJson[0].text)
-  }
+    const additionalJson: Array<ApiAdditioanlData> =
+      await csvToJson().fromString(res);
+    setCourses(additionalJson[0].courses);
+    setAdText(additionalJson[0].text);
+  };
 
-  const categoriesChangeHandle = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const categoriesChangeHandle = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     const category = event.target.value;
 
     setSale(false);
     setMarkdown(false);
 
-    event.target.checked ? selectedCategories.push(category) : selectedCategories.splice(selectedCategories.indexOf(category), 1)
+    event.target.checked
+      ? selectedCategories.push(category)
+      : selectedCategories.splice(selectedCategories.indexOf(category), 1);
 
-    selectedCategories.length === 0 ? setFilteredData(data)
-      : setFilteredData(data?.filter((item) => selectedCategories.includes(item.category)))
-  }
+    selectedCategories.length === 0
+      ? setFilteredData(data)
+      : setFilteredData(
+          data?.filter((item) => {
+            const selectedAutoMarks = selectedCategories.find((cat) =>
+              autoMarks.includes(cat)
+            );
+            const selectedCats = selectedCategories.find((cat) =>
+              categories.includes(cat)
+            );
+
+            const res = selectedCats
+              ? selectedAutoMarks
+                ? selectedCategories.includes(item.category) &&
+                  selectedCategories.includes(item.auto)
+                : selectedCategories.includes(item.category)
+              : selectedCategories.includes(item.auto) 
+
+            return res;
+          })
+        );
+  };
 
   const searchHandle = (event: React.ChangeEvent<HTMLInputElement>) => {
     const text = event.target.value;
 
-    selectedCategories.length === 0 ? setFilteredData(data?.filter((item) => searchTextInObject(text, item)))
-      : setFilteredData(data?.filter((item) => searchTextInObject(text, item) && selectedCategories.includes(item.category)))
-  }
+    selectedCategories.length === 0
+      ? setFilteredData(data?.filter((item) => searchTextInObject(text, item)))
+      : setFilteredData(
+          data?.filter(
+            (item) =>
+              searchTextInObject(text, item) &&
+              selectedCategories.includes(item.category)
+          )
+        );
+  };
 
   const searchTextInObject = (text: string, item: ApiData): boolean => {
     for (let value of Object.values(item)) {
-      const field = value.toLowerCase().replaceAll(/[/,.-\s]/g, '')
-      const textToSearch = text.toLowerCase().replaceAll(/[/,.-\s]/g, '')
+      const field = value.toLowerCase().replaceAll(/[/,.-\s]/g, "");
+      const textToSearch = text.toLowerCase().replaceAll(/[/,.-\s]/g, "");
       if (field.includes(textToSearch)) {
         return true;
       }
     }
 
     return false;
-  }
+  };
 
   const getAllCategories = (data: Array<ApiData>) => {
-    const allCategories: Array<string> = []
+    const allCategories: Array<string> = [];
 
     data.forEach((item) => {
       const itemCategory = item.category;
       if (!allCategories.includes(itemCategory) && itemCategory.length !== 0) {
-        allCategories.push(itemCategory)
+        allCategories.push(itemCategory);
       }
-    })
+    });
 
-    return allCategories
-  }
+    return allCategories;
+  };
+
+  const getAllAutoMarks = (data: Array<ApiData>) => {
+    const allCategories: Array<string> = [];
+
+    data.forEach((item) => {
+      const itemCategory = item.auto;
+      if (!allCategories.includes(itemCategory) && itemCategory.length !== 0) {
+        allCategories.push(itemCategory);
+      }
+    });
+
+    return allCategories;
+  };
 
   const pageHandler = (event: React.MouseEvent<HTMLElement>) => {
     if (!showFiltersCheckbox) {
-      return
+      return;
     }
 
     if (event.target instanceof Element) {
       const classClicked = event.target.className;
 
-      if (classClicked !== 'messageCheckbox' && classClicked !== 'categories' && classClicked !== 'categoryLabel') {
-        setShowFiltersCheckbox(false)
+      if (
+        classClicked !== "messageCheckbox" &&
+        classClicked !== "categories" &&
+        classClicked !== "categoryLabel"
+      ) {
+        setShowFiltersCheckbox(false);
       }
     }
-  }
+  };
 
   const onStockHandle = (stockOption: StockOption) => {
     let newStockValue;
 
     if (stockOption === "акция") {
-      setSale(!sale)
+      setSale(!sale);
       newStockValue = sale;
-      setMarkdown(false)
+      setMarkdown(false);
     } else {
-      setMarkdown(!markdown)
+      setMarkdown(!markdown);
       newStockValue = markdown;
-      setSale(false)
+      setSale(false);
     }
 
     selectedCategories.length === 0
       ? newStockValue
         ? setFilteredData(data)
-        : setFilteredData(data?.filter((item) => searchTextInObject(stockOption, item)))
+        : setFilteredData(
+            data?.filter((item) => searchTextInObject(stockOption, item))
+          )
       : newStockValue
-        ? setFilteredData(data?.filter((item) => selectedCategories.includes(item.category)))
-        : setFilteredData(data?.filter((item) => selectedCategories.includes(item.category) && searchTextInObject(stockOption, item)))
-  }
+      ? setFilteredData(
+          data?.filter((item) => selectedCategories.includes(item.category))
+        )
+      : setFilteredData(
+          data?.filter(
+            (item) =>
+              selectedCategories.includes(item.category) &&
+              searchTextInObject(stockOption, item)
+          )
+        );
+  };
 
   const loadCurrencyFromBank = async () => {
     const data = await fetch("https://www.nbrb.by/api/exrates/rates/431");
-    setCurrency(await data.json())
-  }
+    setCurrency(await data.json());
+  };
 
   const getRublePrice = (price: string): number => {
-    price = price.replace(',', '.')
+    price = price.replace(",", ".");
 
     if (!isNaN(+price) && currency) {
-      return (+price) * (currency.Cur_OfficialRate + +courses!)
+      return +price * (currency.Cur_OfficialRate + +courses!);
     }
 
-    return 0
-  }
+    return 0;
+  };
 
   useEffect(() => {
-    loadDataFromSheet(URL)
-    loadCurrencyFromBank()
-  }, [])
+    loadDataFromSheet(URL);
+    loadCurrencyFromBank();
+  }, []);
 
   return (
     <section onClick={(event) => pageHandler(event)}>
-      <div className='header'>
-        <div className='infoBlock'>
-          <input className='searchInput' onChange={searchHandle} placeholder="Search..." />
+      <header className="header">
+        <div className="infoBlock">
+          <input
+            className="searchInput"
+            onChange={searchHandle}
+            placeholder="Search..."
+          />
           <div>
             <button
-              className='categoriesButton'
+              className="categoriesButton"
               onClick={() => setShowFiltersCheckbox(!showFiltersCheckbox)}
-              style={{ backgroundColor: selectedCategories.length !== 0 ? "yellow" : "white" }}>
-              {showFiltersCheckbox ? 'CLOSE' : 'FILTER'}
+              style={{
+                backgroundColor:
+                  selectedCategories.length !== 0 ? "yellow" : "white",
+              }}
+            >
+              {showFiltersCheckbox ? "CLOSE" : "FILTER"}
             </button>
-            <button className={`salesButton ${sale && 'saleSuccess'}`} onClick={() => onStockHandle("акция")}>
+            <button
+              className={`salesButton ${sale && "saleSuccess"}`}
+              onClick={() => onStockHandle("акция")}
+            >
               Акции
             </button>
-            <button className={`salesButton ${markdown && 'saleSuccess'}`} onClick={() => onStockHandle("уценка")}>
+            <button
+              className={`salesButton ${markdown && "saleSuccess"}`}
+              onClick={() => onStockHandle("уценка")}
+            >
               Уценка
             </button>
-            <div className='categories' style={{ display: showFiltersCheckbox ? "block" : "none" }}>
-              {categories.map((category) =>
-                <label className='categoryLabel'>
-                  <input className="messageCheckbox"
-                    type="checkbox"
-                    value={category}
-                    name="categories"
-                    onChange={categoriesChangeHandle} />
-                  {category} <br />
-                </label>
-              )}
+            <div
+              className="filtering"
+              style={{ display: showFiltersCheckbox ? "block" : "none" }}
+            >
+              <div className="categoriesContainer">
+                <div className="categories">
+                  {categories.map((category) => (
+                    <label className="categoryLabel">
+                      <input
+                        className="messageCheckbox"
+                        type="checkbox"
+                        value={category}
+                        name="categories"
+                        onChange={categoriesChangeHandle}
+                      />
+                      {category}
+                    </label>
+                  ))}
+                </div>
+                <div className="categories">
+                  {autoMarks.map((autoMark) => (
+                    <label className="categoryLabel">
+                      <input
+                        className="messageCheckbox"
+                        type="checkbox"
+                        value={autoMark}
+                        name="categories"
+                        onChange={categoriesChangeHandle}
+                      />
+                      {autoMark}
+                    </label>
+                  ))}
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <AutopartForm show={showProductForm} className='autoPartForm' onClose={() => setShowProductForm(false)}/>
-        <button className='orderButton' onClick={() => setShowProductForm(!showProductForm)}>Заказать</button>
+        <AutopartForm
+          show={showProductForm}
+          className="autoPartForm"
+          onClose={() => setShowProductForm(false)}
+        />
+        <button
+          className="orderButton"
+          onClick={() => setShowProductForm(!showProductForm)}
+        >
+          Заказать
+        </button>
         <span>
           {currency && (currency.Cur_OfficialRate + +courses!).toFixed(2)}
         </span>
-      </div>
-      <div className='section'>
-        <table className='table'>
-          <tr>
-            <th>артикул</th>
-            <th>описание</th>
-            <th>кол-во</th>
-            <th>ц</th>
-          </tr>
-          {filteredData?.map((item) =>
+      </header>
+      <div className="section">
+        <table className="table">
+          <thead>
             <tr>
-              <td>{item["артикул"]}</td>
-              <td>{item["описание"]}</td>
-              <td>{item["кол-во"]}</td>
-              <td>{item["ц"]} {item["ц"] && <span className='rublePrice'>/ {getRublePrice(item["ц"]).toFixed(1)}</span>}</td>
+              <th>артикул</th>
+              <th>описание</th>
+              <th>кол-во</th>
+              <th>ц</th>
             </tr>
-          )}
+          </thead>
+          <tbody>
+            {filteredData?.map((item) => (
+              <tr>
+                <td>{item["артикул"]}</td>
+                <td>{item["описание"]}</td>
+                <td>{item["кол-во"]}</td>
+                <td>
+                  {item["ц"]}{" "}
+                  {item["ц"] && (
+                    <span className="rublePrice">
+                      / {getRublePrice(item["ц"]).toFixed(1)}
+                    </span>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
 
-      <div className='marquee'>
+      <div className="marquee">
         <Marquee speed={60} pauseOnHover={true}>
           {adText}
         </Marquee>
       </div>
     </section>
-  )
-}
+  );
+};
